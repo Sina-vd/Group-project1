3e10a367fed6b320d2029f08f0a2f0715f037df5
rezanadian1365 <rezanadian1365@gmail.com>
2025-04-03 17:08:14 +0330

    test2

--- Group-project1.py
+++ Group-project1.py
@@ -5,11 +5,14 @@ def sum_numbers(numbers):
 a = sum_numbers([1, 2, 3])
 print(a)
 
+
 def len_lst(lst):
     return len(lst)
+
 
 def average(lst):
     return sum_numbers(lst) / len(lst)
 
-print(average([1,2,3,4]))
 
+print(average([1, 2, 3, 4]))
+# test 2
