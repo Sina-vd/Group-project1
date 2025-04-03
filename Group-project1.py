53ddd34cb72da0121862bad11651553691f4b567
sina rezaie <sina.rz1384@gmail.com>
2025-04-03 16:53:08 +0330

    final project

--- Group-project1.py
+++ Group-project1.py
@@ -4,7 +4,7 @@ def sum_numbers(numbers):
 
 a = sum_numbers([1, 2, 3])
 print(a)
-<<<<<<< HEAD
+
 def len_lst(lst):
     return len(lst)
 
@@ -12,5 +12,4 @@ def average(lst):
     return sum_numbers(lst) / len(lst)
 
 print(average([1,2,3,4]))
-=======
->>>>>>> aa6ec67848aff6a7214fb303c1faf99f42ac30d8
+
